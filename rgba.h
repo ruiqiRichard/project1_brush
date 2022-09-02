ec920cbf464b18759503e80906caaef04e4da622
mehekj <mehek.jethani@gmail.com>
2022-09-02 14:52:37 -0400

    init

---
+++ rgba.h
@@ -0,0 +1,12 @@
+#ifndef RGBA_H
+#define RGBA_H
+
+// struct for storing a color using values [0, 255]
+struct RGBA {
+    unsigned char r;
+    unsigned char g;
+    unsigned char b;
+    unsigned char a = 255;
+};
+
+#endif // RGBA_H
---
+++ settings.h
@@ -0,0 +1,80 @@
+/**
+ * @file    Settings.h
+ *
+ * This file contains various settings and enumerations that you will need to use in the various
+ * assignments. The settings are bound to the GUI via static data bindings.
+ */
+
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+#include <QObject>
+#include "rgba.h"
+
+// Enumeration values for the Brush types from which the user can choose in the GUI.
+enum BrushType {
+    BRUSH_CONSTANT,
+    BRUSH_LINEAR,
+    BRUSH_QUADRATIC,
+    BRUSH_SMUDGE,
+    BRUSH_SPRAY,
+    BRUSH_ANGLE,
+    BRUSH_SPEED,
+    BRUSH_FILL,
+    NUM_BRUSH_TYPES
+};
+
+// Enumeration values for the Filters that the user can select in the GUI.
+enum FilterType {
+    FILTER_EDGE_DETECT,
+    FILTER_BLUR,
+    FILTER_SCALE,
+    FILTER_MEDIAN,
+    FILTER_CHROMATIC,
+    FILTER_LEVEL,
+    FILTER_ROTATION,
+    FILTER_BILATERAL,
+    NUM_FILTER_TYPES
+};
+
+/**
+ * @struct Settings
+ *
+ * Stores application settings for the CS123 GUI.
+ *
+ * You can access all app settings through the "settings" global variable.
+ * The settings will be automatically updated when things are changed in the
+ * GUI (the reverse is not true however: changing the value of a setting does
+ * not update the GUI).
+*/
+struct Settings {
+    // Brush
+    int brushType;      // The user's selected brush @see BrushType
+    int brushRadius;    // The brush radius
+    RGBA brushColor;
+    int brushDensity; // This is for spray brush (extra credit)
+    bool fixAlphaBlending; // Fix alpha blending (extra credit)
+
+    // Filter
+    int filterType;                     // The selected filter @see FilterType
+    float edgeDetectSensitivity;    // Edge detection sensitivity, from 0 to 1.
+    int blurRadius;                 // Selected blur radius
+    float scaleX;                   // Horizontal scale factor
+    float scaleY;                   // Vertical scale factor
+    int medianRadius;               // Median radius (extra credit)
+    float rotationAngle;            // Rotation angle (extra credit)
+    int bilateralRadius;            // Bilateral radius (extra credit)
+    float lambda_1;                 // Chromatic aberration labmda 1 (extra credit)
+    float lambda_2;                 // Chromatic aberration labmda 2 (extra credit)
+    float lambda_3;                 // Chromatic aberration labmda 3 (extra credit)
+
+    QString imagePath;
+
+    void loadSettingsOrDefaults();
+    void saveSettings();
+};
+
+// The global Settings object, will be initialized by MainWindow
+extern Settings settings;
+
+#endif // SETTINGS_H
