04ba04f77af3a0e07a6bcc73950987560df00dd2
ruiqiRichard <ruiqiyang.richard@gmail.com>
2024-09-30 02:11:26 -0400

    delete comments

--- filters/filterscale.cpp
+++ filters/filterscale.cpp
@@ -69,104 +69,11 @@ void filterScaleHelper(std::vector<RGBA>& m_data, float factor, int width, int h
 void Canvas2D::filterScale(float scaleX, float scaleY) {
     int new_width = std::round(m_width * scaleX);
     int new_height = std::round(m_height * scaleY);
-    // if (scaleX < 1) {
-    //     blur(2);
-    // }
+
     filterScaleHelper(m_data, scaleX, m_width, m_height, true);
     m_width = new_width;
-
-    // if (scaleY < 1) {
-    //     blur(2);
-    // }
 
     filterScaleHelper(m_data, scaleY, m_width, m_height, false);
     m_height = new_height;
 
-    // float radiusX = scaleX >= 1 ? 1.f : 1 / scaleX;
-    // float radiusY = scaleY >= 1 ? 1.f : 1 / scaleY;
-
-    // std::vector<RGBA> buffer(new_width * m_height, RGBA{0, 0, 0, 255});
-
-    // for (int i = 0; i < m_height; i++) {
-    //     for (int j = 0; j < new_width; j++) {
-    //         int index = i * new_width + j;
-
-    //         float x = j / scaleX + (1-scaleX) / (2 * scaleX);
-    //         float y = i / scaleY + (1-scaleY) / (2 * scaleY);
-
-    //         int x_floor = scaleX >= 1 ? std::ceil(x - 1) : std::ceil(x - 1 / scaleX);
-    //         int y_floor = scaleY >= 1 ? std::ceil(y - 1) : std::ceil(y - 1 / scaleY);
-
-    //         int x_ceil = scaleX >= 1 ? std::floor(x + 1) : std::floor(x + 1 / scaleX);
-    //         int y_ceil = scaleY >= 1 ? std::floor(y + 1) : std::floor(y + 1 / scaleY);
-
-
-    //         double weight_sum = 0;
-    //         double acc_r = 0;
-    //         double acc_g = 0;
-    //         double acc_b = 0;
-
-    //         for (int kx = x_floor; kx <= x_ceil; kx++) {
-    //             RGBA pixel = getPixelRepeated(m_data, m_width, m_height, kx, i);
-    //             double weight = g((x-kx), radiusX);
-    //             weight_sum += weight;
-    //             acc_r += pixel.r * weight;
-    //             acc_g += pixel.g * weight;
-    //             acc_b += pixel.b * weight;
-    //         }
-
-    //         buffer[index].r += static_cast<uint8_t>(acc_r + 0.5);
-    //         buffer[index].g += static_cast<uint8_t>(acc_g + 0.5);
-    //         buffer[index].b += static_cast<uint8_t>(acc_b + 0.5);
-            
-    //         weight_sum = 0;
-    //         acc_r = 0;
-    //         acc_g = 0;
-    //         acc_b = 0;
-
-            // for (int ky = y_floor; ky <= y_ceil; ky++) {
-            //     RGBA pixel = getPixelRepeated(buffer_X, new_width, m_height, j, ky);
-            //     double weight = g((y-ky), radiusY);
-            //     weight_sum += weight;
-            //     acc_r += pixel.r * weight;
-            //     acc_g += pixel.g * weight;
-            //     acc_b += pixel.b * weight;
-            // }
-
-            // buffer[index].r += static_cast<uint8_t>(acc_r + 0.5);
-            // buffer[index].g += static_cast<uint8_t>(acc_g + 0.5);
-            // buffer[index].b += static_cast<uint8_t>(acc_b + 0.5);
-
-
-            // // 2x2 triangle filter
-            // RGBA top_left = getPixelRepeated(m_data, m_width, m_height, x_floor, y_floor);
-            // RGBA top_right = getPixelRepeated(m_data, m_width, m_height, x_ceil, y_floor);
-            // RGBA bottom_left = getPixelRepeated(m_data, m_width, m_height, x_floor, y_ceil);
-            // RGBA bottom_right = getPixelRepeated(m_data, m_width, m_height, x_ceil, y_ceil);
-
-            // buffer[index].r = static_cast<uint8_t>(
-            //     top_left.r * (1 - delta_x) * (1 - delta_y) +
-            //     top_right.r * delta_x * (1 - delta_y) +
-            //     bottom_left.r * (1 - delta_x) * delta_y +
-            //     bottom_right.r * delta_x * delta_y + 0.5
-            // );
-
-            // buffer[index].g = static_cast<uint8_t>(
-            //     top_left.g * (1 - delta_x) * (1 - delta_y) +
-            //     top_right.g * delta_x * (1 - delta_y) +
-            //     bottom_left.g * (1 - delta_x) * delta_y +
-            //     bottom_right.g * delta_x * delta_y + 0.5
-            // );
-
-            // buffer[index].b = static_cast<uint8_t>(
-            //     top_left.b * (1 - delta_x) * (1 - delta_y) +
-            //     top_right.b * delta_x * (1 - delta_y) +
-            //     bottom_left.b * (1 - delta_x) * delta_y +
-            //     bottom_right.b * delta_x * delta_y + 0.5
-            // );
-    //     }
-    // }
-
-
-    // m_height = new_height;
 }
